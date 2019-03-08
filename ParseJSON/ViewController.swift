fca5a880b25d7aba1ae5d6a633bb289d00069680
andreaperez <aap15a@acu.edu>
2019-03-08 08:20:24 -0600

    Initial Commit

---
+++ ParseJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseJSON
+//
+//  Created by Andrea Perez on 3/8/19.
+//  Copyright © 2019 Andrea Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
